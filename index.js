9e976e5367b96c2fe442151ac9ca2e09fa5fccdf
dockrexter <mshaheer.rahi@gmail.com>
2022-09-13 00:49:01 +0500

    minor bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const path = require("path");
 const cors = require("cors");
-const http = require("http");
+const http = require("https");
 const express = require("express");
 const utils = require("./utils/utils");
 const bodyParser = require("body-parser");
