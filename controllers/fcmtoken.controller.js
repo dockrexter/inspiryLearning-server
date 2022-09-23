f3eb7ec7474afd15c1a8a493001e38bb3179d71b
dockrexter <mshaheer.rahi@gmail.com>
2022-09-23 15:49:13 +0500

    bug fix

--- controllers/fcmtoken.controller.js
+++ controllers/fcmtoken.controller.js
@@ -2,37 +2,40 @@ const db = require("../models");
 const response = require("../utils/response");
 
 const add = async (req, res) => {
-  try{
-  const token = await db.FCMToken.create({
-    token: req.body.token,
-    userId: req.user.id,
-  });
-  return res.status(200).json(
-    response(200, "ok", "Token register successfully", {
+  try {
+    const token = await db.FCMToken.create({
       token: req.body.token,
-    })
-  )} catch (error) {
+      userId: req.user.id,
+    });
+    return res.status(200).json(
+      response(200, "ok", "Token register successfully", {
+        token: req.body.token,
+      })
+    )
+  } catch (error) {
+    console.log(error)
     return res.status(500).json(
       response(500, "error", "Something went Wrong"))
   }
 };
 
 const remove = async (req, res) => {
-  try{
-  await db.FCMToken.destroy({
-    where: {
-      token: req.body.token,
-      userId: req.user.id,
-    },
-  });
-  // if (row) {
-  //   await row.destroy();
-  // }
-  return res.status(200).json(
-    response(200, "ok", "Token removed successfully", {
-      token: req.body.token,
-    })
-  )} catch (error) {
+  try {
+    await db.FCMToken.destroy({
+      where: {
+        token: req.body.token,
+        userId: req.user.id,
+      },
+    });
+    // if (row) {
+    //   await row.destroy();
+    // }
+    return res.status(200).json(
+      response(200, "ok", "Token removed successfully", {
+        token: req.body.token,
+      })
+    )
+  } catch (error) {
     return res.status(500).json(
       response(500, "error", "Something went Wrong"))
   }
